cb4d962deeb7e94814cf746e504c0b20dad256c1
hnakano863 <notchi863@gmail.com>
2020-08-31 21:37:02 +0900

    コマンドライン引数とった

--- fizzBuzz/fizzBuzz.go
+++ fizzBuzz/fizzBuzz.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	for i := 1; i <= 30; i++ {
-		if i % 15 == 0 {
+	n, _ := strconv.Atoi(os.Args[1])
+	for i := 1; i <= n; i++ {
+		if i%15 == 0 {
 			fmt.Println("FizzBuzz")
-		} else if i % 5 == 0 {
+		} else if i%5 == 0 {
 			fmt.Println("Buzz")
-		} else if i % 3 == 0 {
+		} else if i%3 == 0 {
 			fmt.Println("Fizz")
 		} else {
 			fmt.Println(i)
